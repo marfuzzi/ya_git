335e336dc7e7e7b0608fbdde2c1ef9c481770437
marfuzzi <mariavershina@gmail.com>
2018-03-28 18:59:58 +0300

    Изменен trevis test 3

--- .hermione.conf.js
+++ .hermione.conf.js
@@ -1,12 +1,11 @@
 module.exports = {
-    baseUrl: 'http://localhost:3000',
-
+    baseUrl: 'http://0.0.0.0:3000',
+    gridUrl: 'http://0.0.0.0:4444/wd/hub',
     sets: {
       desktop: {
           files: 'test/hermione'
       }
     },
-
     browsers: {
       chrome: {
         desiredCapabilities: {
