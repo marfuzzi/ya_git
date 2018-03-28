3181f1667e8d8e6f85e5ad7a42a3516721721746
marfuzzi <mariavershina@gmail.com>
2018-03-29 01:18:47 +0300

    test travis

--- test/unit/helpers/dir.test.js
+++ test/unit/helpers/dir.test.js
@@ -1,21 +1,21 @@
-// const expect = require('chai').expect;
+const expect = require('chai').expect;
 
-// const dir = require('../../../server/helpers/dir');
+const dir = require('../../../server/helpers/dir');
 
-// describe('Dir', () => {
-//     describe('getTypeHashName', () => {
-//         it('Возвращает массив объектов с ключами {type, hash, name}', () => {
-//             const initialString = '100644 blob 8f35cd\tapp.js\n'+
-//                                   '100644 blob 2514af\tpackage.json\n'+
-//                                   '040000 tree 9f2946\tserver';
-//             const result = dir.getTypeHashName(initialString);
-//             const expectedResult = [
-//                 {type: 'blob', hash: '8f35cd', name: 'app.js'},
-//                 {type: 'blob', hash: '2514af', name: 'package.json'},
-//                 {type: 'tree', hash: '9f2946', name: 'server'},
-//             ];
+describe('Dir', () => {
+    describe('getTypeHashName', () => {
+        it('Возвращает массив объектов с ключами {type, hash, name}', () => {
+            const initialString = '100644 blob 8f35cd\tapp.js\n'+
+                                  '100644 blob 2514af\tpackage.json\n'+
+                                  '040000 tree 9f2946\tserver';
+            const result = dir.getTypeHashName(initialString);
+            const expectedResult = [
+                {type: 'blob', hash: '8f35cd', name: 'app.js'},
+                {type: 'blob', hash: '2514af', name: 'package.json'},
+                {type: 'tree', hash: '9f2946', name: 'server'},
+            ];
 
-//             expect(result).to.deep.equal(expectedResult);
-//         });
-//     });
-// });
+            expect(result).to.deep.equal(expectedResult);
+        });
+    });
+});
--- test/unit/utils/execProcess.test.js
+++ test/unit/utils/execProcess.test.js
@@ -1,19 +1,29 @@
-const chai = require('chai');
-const {expect, should} = chai;
+// const chai = require('chai');
+// const {expect, should} = chai;
 
-const myRepo = require('../../../server/config/config').pathToFile;
-const execProcess = require('../../../server/utils/execProcess');
+// const myRepo = require('../../../server/config/config').pathToFile;
+// const execProcess = require('../../../server/utils/execProcess');
 
-describe('execProcess', () => {
-    describe('git log', () => {
-        it('Возвращает промисс', () => {
-            expect(execProcess('git log master --pretty=format:%h', {cwd: `${myRepo}`})).to.be.a('promise');
-        });
-    });
+// describe('execProcess', () => {
+//     describe('git log', () => {
+//         it('Возвращает промисс', () => {
+//             expect(execProcess('git log master --pretty=format:%h', {cwd: `${myRepo}`})).to.be.a('promise');
+//         });
+//         it('resolve(string)', () => {
+//             return execProcess('git log master --pretty=format:%h', {cwd: `${myRepo}`}).then((result) => {
+//                 expect(result).to.be.a('string');
+//             });
+//         });
+//     });
 
-    describe('git branch', () => {
-        it('Возвращает промисс', () => {
-            expect(execProcess('git branch', {cwd: `${myRepo}`})).to.be.a('promise');
-        });
-    });
-});
+//     describe('git branch', () => {
+//         it('Возвращает промисс', () => {
+//             expect(execProcess('git branch', {cwd: `${myRepo}`})).to.be.a('promise');
+//         });
+//         it('resolve(string)', () => {
+//             return execProcess('git branch', {cwd: `${myRepo}`}).then((result) => {
+//                 expect(result).to.be.a('string');
+//             });
+//         });
+//     });
+// });
